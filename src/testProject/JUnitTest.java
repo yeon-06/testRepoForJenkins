0576a105ce97485abba36375c18054b0ed6b6db3
yeon-06 <53105735+yeon-06@users.noreply.github.com>
2021-04-06 10:33:39 +0900

    JUnit 연동 테스트용 테스트

---
+++ src/testProject/JUnitTest.java
@@ -0,0 +1,15 @@
+package testProject;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class JUnitTest {
+
+	@Test
+	public void JUnitTest() {
+		Calculator c = new Calculator();
+		assertEquals(30, c.sum(10,20));
+	}
+
+}
